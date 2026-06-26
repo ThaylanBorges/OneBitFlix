85aa358f7904184b27304c1b0408388007bcafef
Thaylan <taylan-150963@hotmail.com>
2026-06-26 14:04:18 -0300

    chore: configuração inicial do projeto com Express, Sequelize e AdminJS

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { adminJs, adminJsRouter } from "./adminjs/index.js";
+import { sequelize } from "./database/index.js";
+import express from "express";
+
+const app = express();
+app.use(express.static("public"));
+
+app.use(adminJs.options.rootPath, adminJsRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  sequelize.authenticate().then(() => {
+    console.log("db connextion successfull");
+  });
+
+  console.log(`Server started successfuly at port ${PORT}`);
+});
