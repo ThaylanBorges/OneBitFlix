e64e1c698eaf133ec90bd989d07e02ef13ce6c2f
Thaylan <taylan-150963@hotmail.com>
2026-08-03 11:43:02 -0300

    feat(register): adiciona página de cadastro de usuários

---
+++ front-end/src/app/register/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/HeaderGeneric";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Onebitflix - Cadastro",
+};
+
+export default function Register() {
+  return (
+    <main>
+      <Header logoUrl="/" btnUrl="/login" btnText="Fazer Login" />
+    </main>
+  );
+}
